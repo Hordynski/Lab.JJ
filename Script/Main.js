09bc5d67e955ae919022ffe91555dac10dc4d8a2
Zachary Hordynski <zach.hordynski@gmail.com>
2018-12-16 23:40:01 -0500

    My weird looking webpage

---
+++ Script/Main.js
@@ -0,0 +1,16 @@
+var count = 0;
+
+function AddingFunction(){
+    count++;
+console.log(count);
+}
+
+function MinusingFunction(){
+    count--;
+console.log(count);
+}
+
+function SquaringFunction(){
+    count = count * count;
+    console.log(count);
+}
